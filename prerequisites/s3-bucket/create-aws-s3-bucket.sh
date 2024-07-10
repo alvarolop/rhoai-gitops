708f69a873c54acd1cb10d3fcfa4472547b88063
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2024-07-10 16:31:18 +0200

    🚚 Move to prereq folder

--- prerequisites/s3-bucket/create-aws-s3-bucket.sh
+++ prerequisites/s3-bucket/create-aws-s3-bucket.sh
@@ -35,7 +35,7 @@ else
 fi
 
 echo -e "\nCreate the Pipelines Datasource Secret"
-oc process -f ./secret-data-connection-pipelines.yaml \
+oc process -f ./prerequisites/s3-bucket/secret-data-connection-pipelines.yaml \
     --param-file $AWS_VARS_FILE --ignore-unknown-parameters=true \
     -p NOTEBOOK_NAMESPACE=$NOTEBOOK_NAMESPACE \
     -p AWS_S3_BUCKET=$AWS_S3_BUCKET | oc apply -f -
